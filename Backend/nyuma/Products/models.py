aa2f46d9862e306edc10e884babafc84c1a7bcb8
Antonykibet <antonykibet059@gmail.com>
2024-12-16 17:53:30 +0300

    test

--- Backend/nyuma/Products/models.py
+++ Backend/nyuma/Products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+    test= models.CharField()
     name = models.CharField(max_length=200,null=True)
     price = models.IntegerField(null=True)
     canvasJSON = models.JSONField()
