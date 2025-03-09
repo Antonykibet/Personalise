61abd5b10a5175e0906f71d9d64cd1cd64c9e680
Antonykibet <antonykibet059@gmail.com>
2025-03-09 04:38:57 +0300

    Add "feature template" title

--- Frontend/src/pages/LandingPage/featuredTemplates.js
+++ Frontend/src/pages/LandingPage/featuredTemplates.js
@@ -1,4 +1,4 @@
-import { Stack, Box, IconButton, Tooltip, } from "@mui/material"
+import { Stack, Box, IconButton, Tooltip, Typography, } from "@mui/material"
 import ProductCard from "../../components/productCard"
 import ThemeSelector from "../../components/themeSelector";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -8,7 +8,8 @@ export default function FeaturedTemplates({isGiftSection}){
     const [products, setProducts] = useState([])
 
     return(
-        <Stack sx={{width:{xs:'100%',md:'95%'}}} width='100%' spacing={1} alignItems='start'>
+        <Stack sx={{width:{xs:'100%',md:'95%'},mt:2}} width='100%' spacing={1} alignItems='start'>
+            <Typography variant="h5" sx={{fontFamily:'poppins',fontWeight:600,color:'#242424'}}>Featured Templates</Typography>
             <ThemeSelector setResults={setProducts} results={products} isGiftSection={isGiftSection} disableSearch={true}/>
             <Box sx={{display:"flex",width:'100%',alignItems:'center',justifyContent:'start',overflowX:'scroll'}} >
                 {products.map((product)=><ProductCard productDetails={product} />)}
