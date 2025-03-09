9b82001740bd9a25903d707256cf1a0e5695f109
Antonykibet <antonykibet059@gmail.com>
2025-03-09 05:36:33 +0300

    Correct Available items styling on large screens

--- Frontend/src/pages/LandingPage/availablItems.js
+++ Frontend/src/pages/LandingPage/availablItems.js
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 import { getShit } from "../../utils";
 
-const itemsStyle= {width:'45%', display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'start',m:1,p:1}
-const itemImageStyle = {width:'54px',marginRight:'8px',border:'solid transparent',borderRadius:'6px'}
+const itemsStyle= { color:'inherit',textDecoration:'none',display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'start',m:1,p:1}
+const itemImageStyle = {width:'74px',marginRight:'8px',border:'solid transparent',borderRadius:'6px'}
 
 export default function AvailableItems(){
     const [availableItems,setAvailableItems] = useState([])
@@ -15,19 +15,17 @@ export default function AvailableItems(){
     },[])
     return (
         <>
-            <Box sx={{my:'32px',width:'100%',display:'flex',flexWrap:'wrap',justifyContent:'space-around'}} >
+            <Box sx={{my:'32px',width:{xs:'100%',md:'90%'},display:'flex',flexWrap:'wrap',justifyContent:'space-between', alignItems:'start'}} >
                 {availableItems.map((item)=>{
                     return(
-                        <Link style={itemsStyle} to={`/availableItem/${item.id}`} target="_top">
-                            <Paper sx={{width:'100%',display:'flex',flexDirection:'row',alignItems:'center'}}>
-                                    <img style={itemImageStyle} alt={item.name} src={item.thumbnail_image_url} />
-                                    <Typography variant='p' sx={{fontWeight:'bold',color:'#383838'}}>{item.name}</Typography>
-                            </Paper>
-                        </Link>
+                        <Paper sx={{width:{md:'20%',xs:'45%'},m:1,display:'flex',flexDirection:'row',alignItems:'center'}}>
+                            <Link style={itemsStyle} to={`/availableItem/${item.id}`} target="_top">
+                                <img style={itemImageStyle} alt={item.name} src={item.thumbnail_image_url} />
+                                <Typography variant='p' sx={{fontWeight:'bold',color:'#383838'}}>{item.name}</Typography>
+                            </Link>
+                        </Paper>
                     )
                 })}
-                
-                
             </Box>
         </>
     )
--- Frontend/src/pages/LandingPage/featuredTemplates.js
+++ Frontend/src/pages/LandingPage/featuredTemplates.js
@@ -8,7 +8,7 @@ export default function FeaturedTemplates({isGiftSection}){
     const [products, setProducts] = useState([])
 
     return(
-        <Stack sx={{width:{xs:'100%',md:'95%'},mt:2}} width='100%' spacing={1} alignItems='start'>
+        <Stack sx={{width:{xs:'100%',md:'90%'},mt:2}} width='100%' spacing={1} alignItems='start'>
             <Typography variant="h5" sx={{fontFamily:'poppins',fontWeight:600,color:'#242424'}}>Featured Templates</Typography>
             <ThemeSelector setResults={setProducts} results={products} isGiftSection={isGiftSection} disableSearch={true}/>
             <Box sx={{display:"flex",width:'100%',alignItems:'center',justifyContent:'start',overflowX:'scroll'}} >
